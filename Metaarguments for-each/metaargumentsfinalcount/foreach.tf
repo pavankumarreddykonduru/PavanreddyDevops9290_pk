3b21f5222173414b75f12c5401be0d34c73084d3
pavankumarreddykonduru <pavanreddy.cse@gmail.com>
2026-04-28 00:32:56 +0530

    fdsfd

--- Metaarguments for-each/metaargumentsfinalcount/foreach.tf
+++ Metaarguments for-each/metaargumentsfinalcount/foreach.tf
@@ -1,6 +1,6 @@
 resource "local_file" "pet" {
   filename = each.value
   # for_each = var.filename
-  for_each = toset(var.filename)
+  for_each = toset(var.filename) #convers list to set #
   content  = "I love my pets!" 
 }
