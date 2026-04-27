a5039d65d99b1e01bf7fd3a8f90fbf5828a4b97b
pavankumarreddykonduru <pavanreddy.cse@gmail.com>
2026-04-28 00:04:42 +0530

    fdfd

--- metaargumentsfinalcount/variable.tf
+++ metaargumentsfinalcount/variable.tf
@@ -4,7 +4,7 @@ variable "filename" {
              "C:\\Devops\\PavanreddyDevops9290_pk\\pets.txt",
              "C:\\Devops\\PavanreddyDevops9290_pk\\dogs.txt",
              "C:\\Devops\\PavanreddyDevops9290_pk\\cats.txt",
-             "C:\\Devops\\PavanreddyDevops9290_pk\\cows.txt"
+             "C:\\Devops\\PavanreddyDevops9290_pk\\cows.txt",
              "C:\\Devops\\PavanreddyDevops9290_pk\\tiger.txt"
   ]
 
