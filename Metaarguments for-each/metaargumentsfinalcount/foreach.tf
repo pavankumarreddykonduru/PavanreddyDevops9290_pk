0e358c70dd4a8f702c947bd9146988b245359165
pavankumarreddykonduru <pavanreddy.cse@gmail.com>
2026-04-28 00:26:25 +0530

    fsfdsfdf

--- Metaarguments for-each/metaargumentsfinalcount/foreach.tf
+++ Metaarguments for-each/metaargumentsfinalcount/foreach.tf
@@ -1,5 +1,6 @@
 resource "local_file" "pet" {
   filename = each.value
-  for_each = var.filename
+  # for_each = var.filename
+  for_each = toset(var.filename)
   content  = "I love my pets!" 
 }
--- Metaarguments for-each/metaargumentsfinalcount/variable.tf
+++ Metaarguments for-each/metaargumentsfinalcount/variable.tf
@@ -1,5 +1,6 @@
 variable "filename" {
-  type    = set(string)
+  # type    = set(string) # set must be unique #
+  type = list(string)
   default = [
              "C:\\Devops\\PavanreddyDevops9290_pk\\pets.txt",
              "C:\\Devops\\PavanreddyDevops9290_pk\\dogs.txt",
