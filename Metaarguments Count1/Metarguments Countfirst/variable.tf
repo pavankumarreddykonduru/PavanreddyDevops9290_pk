fa6a920486bacd7a16c4a8649a4caaf800a829dc
pavankumarreddykonduru <pavanreddy.cse@gmail.com>
2026-04-27 23:55:46 +0530

    fdfdf

--- Metaarguments Count1/Metarguments Countfirst/variable.tf
+++ Metaarguments Count1/Metarguments Countfirst/variable.tf
@@ -1,5 +1,5 @@
 variable "filename" {
-  type    = tuple
+  type    = list
   default = [
              "C:\\Devops\\PavanreddyDevops9290_pk\\pets.txt",
              "C:\\Devops\\PavanreddyDevops9290_pk\\dogs.txt",
