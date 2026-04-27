7b69c5dff3853b1658124129b477f583950eabef
pavankumarreddykonduru <53435946+pavankumarreddykonduru@users.noreply.github.com>
2026-04-28 00:33:55 +0530

    Update foreach.tf

--- Metaarguments for-each/metaargumentsfinalcount/foreach.tf
+++ Metaarguments for-each/metaargumentsfinalcount/foreach.tf
@@ -1,6 +1,6 @@
 resource "local_file" "pet" {
   filename = each.value
   # for_each = var.filename
-  for_each = toset(var.filename) #convers list to set #
+  for_each = toset(var.filename) #converts list to set #
   content  = "I love my pets!" 
 }
