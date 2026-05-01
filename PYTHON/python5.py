c94fde80e0985424e33f53869e6666d2cf4d9111
pavankumarreddykonduru <pavanreddy.cse@gmail.com>
2026-05-02 01:01:22 +0530

    py

--- PYTHON/python5.py
+++ PYTHON/python5.py
@@ -7,11 +7,15 @@ print(result)
 
 words is a list of strings:
 
-["I", "am", "learning", "Devop"]
+["I", "am", "learning", "Devops"]
 " ".join(words):
 " " → separator (space)
 .join(words) → combines all list elements into one string
 
 So it becomes:
 
-"I am learning Devop" """
+"I am learning Devops" """
+
+""" Key idea
+join() is used to combine a list of strings into a single string
+The string before .join() acts as the separator """
