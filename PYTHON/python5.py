8718020661a84b08335a945e5d515d88fc05a2f0
pavankumarreddykonduru <pavanreddy.cse@gmail.com>
2026-05-02 00:59:20 +0530

    py

---
+++ PYTHON/python5.py
@@ -0,0 +1,17 @@
+words = ["I", "am", "learning", "Devops"]
+result = " ".join(words)
+print(result)
+
+
+""" Explanation
+
+words is a list of strings:
+
+["I", "am", "learning", "Devop"]
+" ".join(words):
+" " → separator (space)
+.join(words) → combines all list elements into one string
+
+So it becomes:
+
+"I am learning Devop" """
