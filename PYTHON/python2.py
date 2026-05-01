7d5961ee7ba7126357319d2e7350daac902d9562
pavankumarreddykonduru <pavanreddy.cse@gmail.com>
2026-05-02 00:30:28 +0530

    py

--- PYTHON/python2.py
+++ PYTHON/python2.py
@@ -1,2 +1,6 @@
 animals = ["dog", "cat", "elephant"]
 print(max(animals))
+
+# max() on a list of strings compares them alphabetically (lexicographically).
+# So "elephant" is considered the largest string.
+# print(max(animals, key=len))
