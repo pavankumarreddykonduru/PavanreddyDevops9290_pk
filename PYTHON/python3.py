8d0962cafd080f756404b7f4a5701a9d72b3a7d5
pavankumarreddykonduru <pavanreddy.cse@gmail.com>
2026-05-02 00:35:48 +0530

    py

---
+++ PYTHON/python3.py
@@ -0,0 +1,10 @@
+result = "3" * 2
+print(result * 2)
+
+""" "3" * 2
+String multiplication repeats the string
+"3" * 2 → "33"
+result * 2
+"33" * 2 → "3333"
+print(result * 2)
+Displays the final string: 3333 """
