755c33b66d45e12d841c183d8b9fd8de014739dc
pavankumarreddykonduru <pavanreddy.cse@gmail.com>
2026-05-02 00:53:53 +0530

    py

--- PYTHON/python4.py
+++ PYTHON/python4.py
@@ -1,3 +1,22 @@
 a, b, c = map(int, input().split())
 print(a, b, c)
 
+""" Explanation
+input() → takes input as a string ("5 10 15")
+.split() → splits into ["5", "10", "15"]
+map(int, ...) → converts each to integers [5, 10, 15]
+a, b, c = ... → assigns values:
+a = 5
+b = 10
+c = 15
+print(a, b, c) → prints them separated by spaces """
+
+""" Why input as string
+Think of input() like reading from a keyboard as text, just like typing in a chat box.
+x = input()
+print(type(x))
+
+input 
+5
+o/p
+<class 'str'> """
